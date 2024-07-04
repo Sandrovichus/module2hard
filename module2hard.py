69ecb0ae6816f730a5470710b14341832fb95305
Alexander <veber21@rambler.ru>
2024-07-04 08:54:13 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,13 @@
+print('Программа для генерации древнего шифра')
+print('______________________________________')
+while True:
+    password = ''
+    number = int(input('Введите любое число от 3 до 20: '))
+    if number > 20 or number < 3:
+        print('Введите число в правильном диапазоне - от 3 до 20!')
+        continue
+    for i in range(1, number):
+        for j in range(i + 1, number):
+            if number % (i + j) == 0:
+                password = password + str(i) + str(j)
+    print(f'Шифр для числа {number}: {password}')
