f87b19c340ef40972c75636bc40afc9e816317e0
Alexander <veber21@rambler.ru>
2024-07-04 08:59:55 +0300

    Add files via upload

--- module2hard.py
+++ module2hard.py
@@ -2,12 +2,12 @@ print('Программа для генерации древнего шифра'
 print('______________________________________')
 while True:
     result = ''
-    number = int(input('Введите любое число от 3 до 20: '))
-    if number > 20 or number < 3:
+    n = int(input('Введите любое число от 3 до 20: '))
+    if n > 20 or n < 3:
         print('Введите число в правильном диапазоне - от 3 до 20!')
         continue
-    for i in range(1, number):
-        for j in range(i + 1, number):
-            if number % (i + j) == 0:
+    for i in range(1, n):
+        for j in range(i + 1, n):
+            if n % (i + j) == 0:
                 result = result + str(i) + str(j)
-    print(f'Шифр для числа {number}: {result}')
+    print(f'Шифр для числа {n}: {result}')
